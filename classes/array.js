2e62b0f4042dd4a59f06c545eedaa5c31f664b7d
Dancode-188 <danbitengo@gmail.com>
2024-09-22 12:24:19 +0300

    Practiced on ES5 Class Syntax and Monkey-patch

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,12 @@
 // Your code here
+Array.prototype.isEqual = function (arr2) {
+    if (this.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] !== arr2[i]) {
+        return false;
+        }
+    }
+    return true;
+}
